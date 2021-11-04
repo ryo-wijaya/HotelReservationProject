a3b03b5fc1dffc44e4f0a0d6bf65a8cbd7462ff1
ryo <ryo200013@gmail.com>
2021-11-04 17:13:57 +0800

    HotelOps module Room operations pt 2

--- HotelReservationProjectManagementClient/src/java/hotelreservationprojectmanagementclient/HotelOperationModule.java
+++ HotelReservationProjectManagementClient/src/java/hotelreservationprojectmanagementclient/HotelOperationModule.java
@@ -356,7 +356,7 @@ public class HotelOperationModule {
     }
 
     private void createNewRoomRate(Scanner sc) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     private void updateARoomRate(Scanner sc) {
