e21742af34922737e0ceb372587ba086f749a442
jordan <jordanwwb29@gmail.com>
2021-11-06 01:13:49 +0800

    Merge origin/master
    Conflicts:
    	HotelReservationProject-ejb/src/java/ejb/session/stateless/RoomTypeSessionBeanLocal.java
    	HotelReservationProjectLibrary/src/ejb/session/stateless/RoomTypeSessionBeanRemote.java

--- HotelReservationProject-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
+++ HotelReservationProject-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
@@ -6,7 +6,15 @@
 package ejb.session.singleton;
 
 import ejb.session.stateless.EmployeeSessionBeanLocal;
+import ejb.session.stateless.RoomRateSessionBeanLocal;
+import ejb.session.stateless.RoomSessionBeanLocal;
+import ejb.session.stateless.RoomTypeSessionBeanLocal;
 import entity.Employee;
+import entity.RoomRate;
+import entity.RoomType;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
@@ -14,7 +22,16 @@ import javax.ejb.LocalBean;
 import javax.ejb.Startup;
 import util.exceptions.EmployeeNotFoundException;
 import util.enumeration.EmployeeRole;
+import static util.enumeration.EmployeeRole.GUESTRELATIONSOFFICER;
+import static util.enumeration.EmployeeRole.OPERATIONMANAGER;
+import static util.enumeration.EmployeeRole.SALESMANAGER;
 import static util.enumeration.EmployeeRole.SYSTEMADMINISTRATOR;
+import util.enumeration.RateType;
+import static util.enumeration.RateType.NORMALRATE;
+import static util.enumeration.RateType.PUBLISHRATE;
+import util.exceptions.RoomNotFoundException;
+import util.exceptions.RoomRateNotFoundException;
+import util.exceptions.RoomTypeNotFoundException;
 
 /**
  *
@@ -25,21 +42,49 @@ import static util.enumeration.EmployeeRole.SYSTEMADMINISTRATOR;
 @Startup
 public class DataInitSessionBean {
 
+    @EJB
+    private RoomRateSessionBeanLocal roomRateSessionBean;
+
+    @EJB
+    private RoomSessionBeanLocal roomSessionBean;
+
+    @EJB
+    private RoomTypeSessionBeanLocal roomTypeSessionBean;
+
     @EJB(name = "EmployeeSessionBeanLocal")
     private EmployeeSessionBeanLocal employeeSessionBeanLocal;
 
+    
+    
     
     @PostConstruct
     public void postConstruct() {
         try {
             employeeSessionBeanLocal.getEmployeeById(1l);
-        } catch (EmployeeNotFoundException ex) {
+            roomRateSessionBean.getRoomRateById(1l);
+            roomTypeSessionBean.getRoomTypeById(1l);
+            roomSessionBean.getRoomById(1l);
+        } catch (EmployeeNotFoundException | RoomRateNotFoundException | RoomTypeNotFoundException | RoomNotFoundException ex) {
             this.loadData();
         }
     }
     
     public void loadData() {
-        Employee admin = new Employee("randomname", "admin", "password", SYSTEMADMINISTRATOR);
-        employeeSessionBeanLocal.createNewEmployee(admin);
+        Employee employee = new Employee("admin", "sysadmin", "password", SYSTEMADMINISTRATOR);
+        employeeSessionBeanLocal.createNewEmployee(employee);
+        employee = new Employee("op", "opmanager", "password", OPERATIONMANAGER);
+        employeeSessionBeanLocal.createNewEmployee(employee);
+        employee = new Employee("sales", "salesmanager", "password", SALESMANAGER);
+        employeeSessionBeanLocal.createNewEmployee(employee);
+        employee = new Employee("guest", "guestrelo", "password", GUESTRELATIONSOFFICER);
+        employeeSessionBeanLocal.createNewEmployee(employee);
+        
+        RoomType roomType = new RoomType("Grand Suite", 1, "Grand Suite", "GRAND", 3, 6, new ArrayList<>());
+        RoomRate publishRoomRate = new RoomRate(PUBLISHRATE, 100.00, null, null);
+        RoomRate normalRoomRate = new RoomRate(NORMALRATE, 50.00, null, null);
+        roomType.addToListOfRoomRate(publishRoomRate);
+        roomType.addToListOfRoomRate(normalRoomRate);
+        roomTypeSessionBean.createNewRoomType(roomType);
+        
     }
 }
