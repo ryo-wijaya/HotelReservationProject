546cab4e7a1a87a6fc68334429f61515f4fe1217
jordan <jordanwwb29@gmail.com>
2021-11-11 14:20:19 +0800

    Merge origin/master
    Conflicts:
    	HotelReservationProject-ejb/src/java/ejb/session/stateless/BookingSessionBean.java
    	HotelReservationProject-ejb/src/java/ejb/session/stateless/BookingSessionBeanLocal.java
    	HotelReservationProjectLibrary/src/ejb/session/stateless/BookingSessionBeanRemote.java
    	HotelReservationProjectReservationClient/src/java/hotelreservationprojectreservationclient/MainApp.java

--- HotelReservationProject-ejb/src/java/ejb/session/ws/WebServiceSessionBean.java
+++ HotelReservationProject-ejb/src/java/ejb/session/ws/WebServiceSessionBean.java
@@ -5,10 +5,17 @@
  */
 package ejb.session.ws;
 
+import ejb.session.stateless.BookingSessionBeanLocal;
+import ejb.session.stateless.PartnerSessionBeanLocal;
+import ejb.session.stateless.RoomSessionBeanLocal;
+import ejb.session.stateless.RoomTypeSessionBeanLocal;
+import entity.Partner;
+import javax.ejb.EJB;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
+import util.exceptions.LoginCredentialsInvalidException;
 
 /**
  *
@@ -18,11 +25,26 @@ import javax.ejb.Stateless;
 @Stateless()
 public class WebServiceSessionBean {
 
+    @EJB
+    private RoomTypeSessionBeanLocal roomTypeSessionBean;
+
+    @EJB
+    private RoomSessionBeanLocal roomSessionBean;
+
+    @EJB
+    private PartnerSessionBeanLocal partnerSessionBean;
+
+    @EJB
+    private BookingSessionBeanLocal bookingSessionBean;
+
+    
+    
     /**
      * This is a sample web service operation
      */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    @WebMethod(operationName = "doLogin")
+    public Partner doLogin(@WebParam String username, @WebParam String password) throws LoginCredentialsInvalidException {
+        return partnerSessionBean.partnerLogin(username, password);
     }
+    
 }
